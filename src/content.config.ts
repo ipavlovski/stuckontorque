db1a365db052b95dc1663c06cf165dd41738466f
IP <ilya.pavlovski@gmail.com>
2026-03-14 19:44:46 -0300

    Get type inferral from collections.

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,6 @@ import { defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
 import { z } from 'astro/zod'
 
-
-// import type { ImageMetadata } from 'astro';
-// export const images = glob({ pattern: ["**/*.png"], base: "./src/posts" })
-
-
 const posts = defineCollection({
   loader: glob({ pattern: ["**/*.md"], base: "./src/posts" }),
   schema: z.object({
